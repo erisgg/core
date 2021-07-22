c5b6f39f1a29d44028df0530b7860ac24e4efa88
hughjph <hughjohnson2018@outlook.com>
2021-07-22 23:59:29 +0100

    redis impl on message command

--- src/main/java/gg/eris/core/ErisCore.java
+++ src/main/java/gg/eris/core/ErisCore.java
@@ -1,8 +1,14 @@
 package gg.eris.core;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import gg.eris.commons.bukkit.ErisBukkitCommons;
+import gg.eris.commons.bukkit.util.CC;
+import gg.eris.commons.core.redis.RedisSubscriber;
+import gg.eris.commons.core.redis.RedisWrapper;
 import gg.eris.core.command.BroadcastCommand;
 import gg.eris.core.command.HubCommand;
+import gg.eris.core.command.MessageCommand;
 import gg.eris.core.command.TeleportCommand;
 import gg.eris.core.command.gamemode.GameModeAdventureCommand;
 import gg.eris.core.command.gamemode.GameModeCreativeCommand;
@@ -14,18 +20,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.UUID;
 
 public final class ErisCore extends JavaPlugin {
 
   public static HashMap<Player, BukkitTask> teleportHubList = new HashMap<>();
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+  private RedisWrapper wrapper;
 
   @Override
   public void onEnable() {
     ErisBukkitCommons commons =
         Bukkit.getServicesManager().getRegistration(ErisBukkitCommons.class).getProvider();
+    wrapper = commons.getRedisWrapper();
     commons.getCommandManager().registerCommands(
         new TeleportCommand(),
         new GamemodeCommand(),
@@ -34,8 +42,31 @@ public final class ErisCore extends JavaPlugin {
         new GameModeAdventureCommand(),
         new GameModeSpectatorCommand(),
         new BroadcastCommand(),
-        new HubCommand()
+        new HubCommand(),
+        new MessageCommand(wrapper)
     );
+
+
+    receiveMessages();
+  }
+
+  private void receiveMessages(){
+    Bukkit.getScheduler().runTaskAsynchronously(this, new Runnable() {
+      @Override
+      public void run() {
+        RedisSubscriber subscriber = RedisSubscriber.builder("message").withCallback(callback -> {
+          JsonNode node = callback.getPayload();
+          String sender = node.get("sender").asText();
+          String receiver = node.get("receiver").asText();
+          String message = node.get("message").asText();
+
+          Player receiverPlayer = Bukkit.getPlayer(UUID.fromString(receiver));
+
+          receiverPlayer.sendMessage(CC.GREEN.underline() + "FROM: " + CC.GOLD.underline() + sender + " " + CC.WHITE + message);
+        }).build();
+        wrapper.subscribe(subscriber);
+      }
+    });
   }
 
 }
--- src/main/java/gg/eris/core/command/MessageCommand.java
+++ src/main/java/gg/eris/core/command/MessageCommand.java
@@ -1,22 +1,33 @@
 package gg.eris.core.command;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import gg.eris.commons.bukkit.command.Command;
 import gg.eris.commons.bukkit.command.CommandManager;
 import gg.eris.commons.bukkit.command.CommandProvider;
-import gg.eris.commons.bukkit.command.argument.Argument;
 import gg.eris.commons.bukkit.command.argument.PlayerArgument;
 import gg.eris.commons.bukkit.command.argument.StringArgument;
-import gg.eris.commons.bukkit.permission.Permission;
-import gg.eris.commons.bukkit.util.CC;
 import gg.eris.commons.core.identifier.Identifier;
+import gg.eris.commons.core.redis.RedisPublisher;
+import gg.eris.commons.core.redis.RedisWrapper;
+import gg.eris.core.ErisCore;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
+
 public class MessageCommand implements CommandProvider {
 
     public static final Identifier PERMISSION = Identifier.of("eris", "message");
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private RedisWrapper wrapper;
+
+
+    public MessageCommand(RedisWrapper wrapper){
+        this.wrapper = wrapper;
+    }
+
 
     @Override
     public Command.Builder getCommand(CommandManager manager) {
@@ -40,7 +51,18 @@ public class MessageCommand implements CommandProvider {
                     Player sender = context.getSenderAsPlayer();
                     Player receiver = context.getArgument("player");
 
-                    receiver.sendMessage(CC.GREEN.underline() + "FROM: " + CC.GOLD.underline() + sender.getName() + CC.WHITE + message);
+                    ObjectNode node = MAPPER.createObjectNode();
+                    node.put("sender", sender.getName());
+                    node.put("receiver", String.valueOf(receiver.getUniqueId()));
+                    node.put("message", message);
+
+                    Bukkit.getScheduler().runTaskAsynchronously(ErisCore.getPlugin(ErisCore.class), new Runnable() {
+                        @Override
+                        public void run() {
+                            RedisPublisher publisher = RedisPublisher.builder(node, "message").build();
+                            wrapper.publish(publisher);
+                        }
+                    });
 
                 }).finished();
     }
